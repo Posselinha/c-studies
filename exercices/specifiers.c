6b72936ac6ebe396b63dbf28110345feea3c0b99
Gabriel Possela <80076413+Posselinha@users.noreply.github.com>
2025-08-10 18:36:40 -0300

    Create specifiers.c

---
+++ exercices/specifiers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n1, n2, n3, n4;
+    float f1, f2, f3, f4;
+
+    printf("%d %d %d %d\n", n1, n2, n3, n4);
+    printf("%f %f %f %f\n", f1, f2, f3, f4);
+
+    return 0;
+}
