bebc167b563942b5a26d98336c01c08b2bb6726b
Gabriel Possela <80076413+Posselinha@users.noreply.github.com>
2025-08-10 18:52:46 -0300

    Create comparison.c

---
+++ exercices/comparison.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i, j, k;
+    i = 2; j = 3;
+    k = i * j == 6;
+    printf("%d\n", k);
+
+    i = 5; j = 10; k = 1;
+    printf("%d\n", k > i < j);
+
+    i = 3; j = 2; k = 1;
+    printf("%d\n", i < j == j < k);
+}
